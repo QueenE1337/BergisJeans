20e4b3c69f015b1dee0184464d8bc72d141e208e
Jacob <jacobduvander@gmail.com>
2017-11-28 14:15:28 +0100

    Added nav container :boom:

--- functions.php
+++ functions.php
@@ -2,5 +2,5 @@
 
   require('includes/reg_post_types.php');
   require('includes/reg_taxonomies.php');
-
+  require('includes/reg_page_menu.php');
  ?>
--- header.php
+++ header.php
@@ -11,6 +11,7 @@
 
 <header>
 
+
 	<?php echo ('hello'); ?>
 
 </header>
