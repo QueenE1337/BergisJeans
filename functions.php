333846834477d11967b35e1d225e2b50bf3ac640
erki16 <32167527+erki16@users.noreply.github.com>
2017-11-28 14:58:17 +0100

    semicolon

--- functions.php
+++ functions.php
@@ -8,5 +8,5 @@
   add_image_size( 'mobile_grid', 500, 500, true );
   add_image_size( 'desktop_single', 800, 800, false );
   add_theme_support('post-thumbnails');
-  add_theme_support('title-tag')
+  add_theme_support('title-tag');
  ?>
