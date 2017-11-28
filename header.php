2d420dfa5ec74374ac519b083d030862ba0e44d7
Jacob <jacobduvander@gmail.com>
2017-11-28 14:28:00 +0100

    unfucked my shit

--- header.php
+++ header.php
@@ -3,10 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<title><?php bloginfo('name') ?></title>
-	<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?> <!-- Printing out the header menu -->
-	<h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name') ?></a></h1> <!-- "logo" with a link to the front page -->
-
-	
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>">
 	<?php wp_head() ?>
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 </head>
@@ -14,6 +11,7 @@
 
 <header>
 
-	<?php echo ('hello'); ?>
+	<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?> <!-- Printing out the header menu -->
+	<h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name') ?></a></h1> <!-- "logo" with a link to the front page -->
 
 </header>
