cb7dc4fa05f52203e0c1210faed9a4e7e4051a71
Jacob <jacobduvander@gmail.com>
2017-11-28 14:59:47 +0100

    Added taxonomies

--- includes/reg_taxonomies.php
+++ includes/reg_taxonomies.php
@@ -1,3 +1,32 @@
 <?php
-  //empty
+function create_project_taxonomies() {
+
+	$args = array(
+    'label'                     => 'Material',
+    'hierarchical'               => false,
+    'public'                     => true,
+  );
+//'product_material', ' product_size', ' product_model', ' product_sex'
+  register_taxonomy( 'product_material', array('products'), $args );
+
+  $args = array(
+    'label'                     => 'Model',
+    'hierarchical'               => false,
+    'public'                     => true,
+  );
+
+  register_taxonomy( 'product_model', array('products'), $args );
+
+  $args = array(
+    'label'                     => 'Gender',
+    'hierarchical'               => false,
+    'public'                     => true,
+  );
+
+  register_taxonomy( 'product_sex', array('products'), $args );
+
+}
+
+add_action( 'init', 'create_project_taxonomies' );
+
  ?>
