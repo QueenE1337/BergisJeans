1402cf07675bfc53e7654434dfbc2b563ddcddd3
erki16 <32167527+erki16@users.noreply.github.com>
2017-11-28 14:55:28 +0100

    added image sizes post thumbnail
    (Y)

--- functions.php
+++ functions.php
@@ -3,4 +3,10 @@
   require('includes/reg_post_types.php');
   require('includes/reg_taxonomies.php');
   require('includes/reg_page_menu.php');
+
+  add_image_size( 'desktop_grid', 300, 300, true );
+  add_image_size( 'mobile_grid', 500, 500, true );
+  add_image_size( 'desktop_single', 800, 800, false );
+  add_theme_support('post-thumbnails');
+  add_theme_support('title-tag')
  ?>
