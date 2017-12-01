ba99372b1fcd631b59be71e2d5e43abb50ce82b3
QueenE1337 <emmaradhall@wlanguest141252.eduroam.hj.se>
2017-12-01 11:13:15 +0100

    Footer looks lit now 🔥🔥

--- footer.php
+++ footer.php
@@ -1,8 +1,34 @@
 <footer>
 
-  <p>© Copyright Bergis Jeans 2017</p>
+
+	<div id="infoLeft">
+		<h3>Social Media:</h3>
+		<p>Facebook</p>
+		<p>Instagram</p>
+		<p>YouTube</p>
+	</div>
+
+	<div id="infoCenter">
+		<h3>Contact:</h3>
+		<p>info@bergisjeans.se</p>
+		<p>0722528052</p>
+		<p>www.bergisjeans.se</p>
+	</div>
+
+	<div id="infoRight">
+		<h3>Sign up to our newsletter:</h3>
+		<form id="smallSignUp">
+			<input type="text" name="subscribe" placeholder="name">
+			<input type="text" name="subscribe" placeholder="mail">
+			<button type="submit" form="smallSignUp"><i class="fa fa-envelope" aria-hidden="true""></i></button>
+		</form>
+	</div>
+
+
+	<p id="copyText"><i>© Copyright Bergis Jeans 2017</i></p>
 
 </footer>
+
 <?php wp_footer(); ?>
 
 </body>
