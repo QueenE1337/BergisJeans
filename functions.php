ffd82dcaab9111abbee032e2386258c776ab16e0
erki16 <32167527+erki16@users.noreply.github.com>
2017-12-04 17:24:53 +0100

    Materials example advanced custom field😭

--- functions.php
+++ functions.php
@@ -1,4 +1,46 @@
+
 <?php
+
+if(function_exists("register_field_group"))
+{
+	register_field_group(array (
+		'id' => 'acf_material',
+		'title' => 'Material',
+		'fields' => array (
+			array (
+				'key' => 'field_5a25660ca2447',
+				'label' => 'Material',
+				'name' => 'Material',
+				'type' => 'checkbox',
+				'required' => 1,
+				'choices' => array (
+					'mocka' => 'Mocka',
+					'denim' => 'Denim',
+				),
+				'default_value' => '',
+				'layout' => 'vertical',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'products',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+}
 
   require('includes/reg_post_types.php');
   require('includes/reg_taxonomies.php');
