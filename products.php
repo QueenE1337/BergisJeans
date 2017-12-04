4fc315f360a032cb2e7dd62100ba6bd11479cda8
Swindian95 <mihir.chouhan1@gmail.com>
2017-12-04 16:36:01 +0100

    fixed the values icons

--- products.php
+++ products.php
@@ -6,9 +6,8 @@
 get_header(); ?>
 
   <main>
-    <h1> This is the products template</h1>
     <div id="icons">
-      <div class="values"><i class="fa fa-globe" aria-hidden="true"></i><p><strong>Environmental Friendly</strong></p></div>
+      <div class="values"><i class="fa fa-globe" aria-hidden="true"></i><p><strong>Environmentally Friendly</strong></p></div>
       <div class="values"><i class="fa fa-star" aria-hidden="true"></i><p><strong>Top Quality</strong></p></div>
       <div class="values"><i class="fa fa-arrows-h" aria-hidden="true"></i><p><strong>Well-Fitted</strong></p></div>
     </div>
