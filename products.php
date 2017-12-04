605442a8dca3ebbb7a8e4389491b2a00c54945b4
Swindian95 <mihir.chouhan1@gmail.com>
2017-12-04 14:25:12 +0100

    the values

--- products.php
+++ products.php
@@ -6,7 +6,14 @@
 get_header(); ?>
 
   <main>
-    <h1> This is the products template</h1>
+    <div id="icons">
+      <div class="values"><i class="fa fa-globe" aria-hidden="true"></i><p><strong>Environmental Friendly</strong></p></div>
+      <div class="values"><i class="fa fa-star" aria-hidden="true"></i><p><strong>Top Quality</strong></p></div>
+      <div class="values"><i class="fa fa-arrows-h" aria-hidden="true"></i><p><strong>Well-Fitted</strong></p></div>
+    </div>
+
+
+
   </main>
 
 <?php get_footer(); ?>
