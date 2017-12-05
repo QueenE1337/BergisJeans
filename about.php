31c802576c076d095a89952a0755adc0f38a670e
Jacob <jacobduvander@gmail.com>
2017-12-05 18:43:10 +0100

    Added img blahahah

--- about.php
+++ about.php
@@ -8,6 +8,7 @@ get_header(); ?>
   <main>
     <h1><?php the_field( "description_title" ); ?></h1>
     <p><?php the_field( "description" ); ?></p>
+    <?php the_post_thumbnail('mobile_grid'); ?>
     <ul class="values">
       <li>
         <h4>Vision</h4>
@@ -23,8 +24,6 @@ get_header(); ?>
       <h4><?php the_field( "history_label" ); ?></h4>
       <p><?php the_field( "history_description" ); ?></p>
     </div>
-
-
   </main>
 
 <?php get_footer(); ?>
