519511ecdc7228f28449fd485bca3f1e85b620ca
Jacob <jacobduvander@gmail.com>
2017-12-05 18:17:41 +0100

    removed unecessary stuff

--- about.php
+++ about.php
@@ -8,6 +8,23 @@ get_header(); ?>
   <main>
     <h1><?php the_field( "description_title" ); ?></h1>
     <p><?php the_field( "description" ); ?></p>
+    <ul class="values">
+      <li>
+        <h4>Vision</h4>
+        <p><?php the_field( "vision" ); ?></p>
+      </li>
+      <li>
+        <h4>Mission</h4>
+        <p><?php the_field( "mission" ); ?></p>
+      </li>
+    </ul>
+
+    <div class="timeline">
+      <h4><?php the_field( "history_label" ); ?></h4>
+      <p><?php the_field( "history_description" ); ?></p>
+    </div>
+
+
   </main>
 
 <?php get_footer(); ?>
