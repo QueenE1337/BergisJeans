3a08feeede3c29924d6c12bd71347785a0ff313f
Jacob <jacobduvander@gmail.com>
2017-12-06 13:07:54 +0100

    Small tweak to post-grid

--- partials/postgrid.php
+++ partials/postgrid.php
@@ -5,12 +5,9 @@
   if(have_posts() ):
   echo "<ul class='news-grid'>";
      while ( have_posts() ) : the_post(); ?>
-
-     <li>
-         <a href="<?php the_permalink();?>"><?php the_title(); ?> </a>
-     </li>
       <li>
           <a href="<?php the_permalink();?>"><?php the_post_thumbnail('desktop_grid'); ?> </a>
+          <a href="<?php the_permalink();?>"><?php the_title(); ?> </a>
       </li>
       <?php endwhile;
         echo "</ul>";
