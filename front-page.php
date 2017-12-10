0d091e5db82ae9c836aedebb81cb484cffc2f393
erki16 <32167527+erki16@users.noreply.github.com>
2017-12-10 12:57:12 +0100

    Displaying news feed on front page

--- front-page.php
+++ front-page.php
@@ -1,7 +1,32 @@
 <?php get_header(); ?>
-
 <main>
-  <h1>Frontpage</h1>
+
+
+<!-- This is where we print out our News section -->
+<h1>News</h1>
+<?php
+$args = array(
+  'post_type' => 'news',
+  'posts_per_page' => 2
+);
+?>
+<?php echo "<ul class='post-grid'>";  ?>
+
+<?php
+$mq=new WP_Query($args);
+if($mq->have_posts() ):
+   while ( $mq->have_posts() ) : $mq->the_post(); ?>
+   <li>
+       <a href="<?php the_permalink();?>"><?php the_post_thumbnail('desktop_grid'); ?> </a>
+       <a href="<?php the_permalink();?>"><?php the_title(); ?> </a>
+   </li>
+
+<?php
+endwhile;
+endif;
+?>
+
+</ul>
 </main>
 
 
