9af7ca1044c380ea99bf3906a021eba29c563e63
Jacob <jacobduvander@gmail.com>
2017-12-11 18:02:03 +0100

    Removed unecessary widgets

--- includes/widget.php
+++ includes/widget.php
@@ -1,22 +1,6 @@
 <?php
 
  function add_widgets() {
-  $args = array(
-    'id'            => 'frontpage-info',
-    /** Visible name in the Admin Dashboard Widget page */
-    'name'          => __( 'Frontpage Info', 'post_theme' ),
-    /** Visible description in the Admin Dashboard Widget page */
-    'description'   => __( 'Frontpage information including some bragging about the company.', 'post_theme' ),
-
-    /** HTML to wrap widget title in  */
-    'before_title'  => '<h2 class="frontpage-info">',
-    'after_title'   => '</h2>',
-    /** HTML to wrap each widget  */
-    'before_widget' => '<section>',
-    'after_widget'  => '</section>',
-  );
-  register_sidebar( $args );
-
  $args = array(
     'id'            => 'subscribe-form',
     /** Visible name in the Admin Dashboard Widget page */
@@ -32,71 +16,6 @@
     'after_widget'  => '</section>',
   );
   register_sidebar( $args );
-
-  $args = array(
-     'id'            => 'ceo-info',
-     /** Visible name in the Admin Dashboard Widget page */
-     'name'          => __( 'CEO Info', 'post_theme' ),
-     /** Visible description in the Admin Dashboard Widget page */
-     'description'   => __( 'A short decription of Annelie', 'post_theme' ),
-
-     /** HTML to wrap widget title in  */
-     'before_title'  => '<h3>',
-     'after_title'   => '</h3>',
-     /** HTML to wrap each widget  */
-     'before_widget' => '<section>',
-     'after_widget'  => '</section>',
-   );
-   register_sidebar( $args );
-
-   $args = array(
-      'id'            => 'contact-time',
-      /** Visible name in the Admin Dashboard Widget page */
-      'name'          => __( 'Contact time', 'post_theme' ),
-      /** Visible description in the Admin Dashboard Widget page */
-      'description'   => __( 'Write your contact information when you are available', 'post_theme' ),
-
-      /** HTML to wrap widget title in  */
-      'before_title'  => '<h2>',
-      'after_title'   => '</h2>',
-      /** HTML to wrap each widget  */
-      'before_widget' => '<section>',
-      'after_widget'  => '</section>',
-    );
-    register_sidebar( $args );
-
-    $args = array(
-      'id'            => 'footer-contact',
-      /** Visible name in the Admin Dashboard Widget page */
-      'name'          => __( 'Contact in footer', 'post_theme' ),
-      /** Visible description in the Admin Dashboard Widget page */
-      'description'   => __( 'The contact information in the footer.', 'post_theme' ),
-
-      /** HTML to wrap widget title in  */
-      'before_title'  => '<h2>',
-      'after_title'   => '</h2>',
-      /** HTML to wrap each widget  */
-      'before_widget' => '<section>',
-      'after_widget'  => '</section>',
-    );
-    register_sidebar( $args );
-
-
-    $args = array(
-      'id'            => 'footer-socialmedia',
-      /** Visible name in the Admin Dashboard Widget page */
-      'name'          => __( 'Social Media in footer', 'post_theme' ),
-      /** Visible description in the Admin Dashboard Widget page */
-      'description'   => __( 'The social media information in the footer.', 'post_theme' ),
-
-      /** HTML to wrap widget title in  */
-      'before_title'  => '<h2>',
-      'after_title'   => '</h2>',
-      /** HTML to wrap each widget  */
-      'before_widget' => '<section>',
-      'after_widget'  => '</section>',
-    );
-    register_sidebar( $args );
 
  }
  add_action( 'widgets_init', 'add_widgets' );
